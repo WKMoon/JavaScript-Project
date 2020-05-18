b035c9eb26254eb60fba8be8921345f75db6a939
Wookyung Moon <wkmoon9601@gmail.com>
2020-05-18 17:00:26 -0300

    complete

--- OpenLibrary/bookData.js
+++ OpenLibrary/bookData.js
@@ -16,8 +16,6 @@ const bookBorrow = rightPanel.querySelector("#borrow_url");
 
 const body = document.querySelector("body");
 
-console.log(posterImg);
-console.log(bookTitle);
 
 function getData(){
     const poster = localStorage.getItem("poster");
@@ -27,34 +25,49 @@ function getData(){
     bookTitle.innerText = title;
 
     const author = localStorage.getItem("author");
-    bookAuthor.innerText = `Author - ${author}`;
+    bookAuthor.innerText = `Author: ${author}`;
 
     const publisher = localStorage.getItem("publisher");
-    bookPublisher.innerText = `Publisher - ${publisher}`;
+    bookPublisher.innerText = `Publisher: ${publisher}`;
 
     const pages = localStorage.getItem("pages");
-    bookPages.innerText = `Pages - ${pages}`;
+    bookPages.innerText = `Pages: ${pages} p`;
 
     const subjects = localStorage.getItem("subjects");
-    bookSubjects.innerText = `Subjects - ${subjects}`;
+    bookSubjects.innerText = `Subjects: ${subjects}`;
 
     const pubDate = localStorage.getItem("pubDate");
-    bookPubDate.innerText = `Publish Date - ${pubDate}`;
+    bookPubDate.innerText = `Publish Date: ${pubDate}`;
 
     const weight = localStorage.getItem("weight");
-    bookWeight.innerText = `Weight - ${weight}`;
-
+    if(weight === "undefined"){
+        bookWeight.innerText = "Weight: Currently Invalid";
+    }
+    else{
+        bookWeight.innerText = `Weight: ${weight}`;
+    }   
+    
     const borrowable = localStorage.getItem("borrowable");
-    bookBorrowable.innerText = `Borrowable - ${borrowable}`;
+    bookBorrowable.innerText = `Borrowable: ${borrowable}`;
 
     const preview_url = localStorage.getItem("preview_url");
-    bookPreview.setAttribute("href",preview_url);
-    bookPreview.innerText = "Preview Link";
+    if(preview_url === "undefined"){
+        bookPreview.innerText = "Proview link is currently invalid";
+    }
+    else{
+        bookPreview.setAttribute("href",preview_url);
+        bookPreview.innerText = "Here";
+    }
 
     const borrow_url = localStorage.getItem("borrow_url");
-    bookBorrow.setAttribute("href",borrow_url);
-    bookBorrow.innerText = "Borrow Link";
-
+    if(borrow_url === "undefined"){
+        bookBorrow.innerText = `Currently 
+        invalid`;
+    } 
+    else{
+        bookBorrow.setAttribute("href",borrow_url);
+        bookBorrow.innerText = "Here";
+    }
 
 
     rightPanel.appendChild(author);
--- OpenLibrary/search.js
+++ OpenLibrary/search.js
@@ -1,23 +1,16 @@
 const isbnForm = document.querySelector(".isbn");
 const isbnInput = isbnForm.querySelector("input");
 
-const oclcForm = document.querySelector(".oclc");
-const oclcInput = oclcForm.querySelector("input");
-
-const lccnForm = document.querySelector(".lccn");
-const lccnInput = lccnForm.querySelector("input");
-
-const olidForm = document.querySelector(".olid");
-const olidInput = olidForm.querySelector("input");
-
 const body = document.querySelector("body");
 
 
 function showISBNData(){
     const isbn = isbnInput.value;
-    console.log(isbn);
     fetch(`https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=data&format=json`)
-    .then(function(response){
+    .then(response => {
+        if(!response.ok){
+            throw new Error('Network response was not ok');
+        }
         return response.json();
     })
     .then(function(json){
@@ -26,6 +19,7 @@ function showISBNData(){
         value = json[key]
 
         const bookInfo = value
+        
         const poster = bookInfo.cover.large;
         const title = bookInfo.title;
         const author = bookInfo.authors[0].name;
@@ -50,135 +44,11 @@ function showISBNData(){
         localStorage.setItem("preview_url",preview_url);
         localStorage.setItem("borrow_url",borrow_url);
 
-
         window.location = "bookData.html";
     })
+    .catch(error => {
+        alert(`Cannot find the ISBN information detail error: ${error}`);
+      });
+
 }
 
-function showOCLCData(){
-    const oclc = oclcInput.value;
-    console.log(oclc);
-    fetch(`https://openlibrary.org/api/books?bibkeys=OCLC:${oclc}&jscmd=data&format=json`)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(json){
-        const idx = 0;
-        const key = Object.keys(json)[idx];
-        value = json[key]
-
-        const bookInfo = value
-        const poster = bookInfo.cover.large;
-        const title = bookInfo.title;
-        const author = bookInfo.authors[0].name;
-        const publisher = bookInfo.publishers[0].name;
-        const pages = bookInfo.number_of_pages;
-        const subjects = bookInfo.subjects[0].name;
-        const pubDate = bookInfo.publish_date;
-        const weight = bookInfo.weight;
-        const borrowable = bookInfo.ebooks[0].availability;
-        const preview_url = bookInfo.ebooks[0].preview_url;
-        const borrow_url = bookInfo.ebooks[0].borrow_url;
-
-        localStorage.setItem("title",title);
-        localStorage.setItem("poster",poster);
-        localStorage.setItem("author",author);
-        localStorage.setItem("publisher",publisher);
-        localStorage.setItem("pages",pages);
-        localStorage.setItem("subjects",subjects);
-        localStorage.setItem("pubDate",pubDate);
-        localStorage.setItem("weight",weight);
-        localStorage.setItem("borrowable", borrowable);
-        localStorage.setItem("preview_url",preview_url);
-        localStorage.setItem("borrow_url",borrow_url);
-
-
-        window.location = "bookData.html";
-
-    })
-}
-
-function showLCCNData(){
-    const lccn = lccnInput.value;
-    console.log(lccn);
-    fetch(`https://openlibrary.org/api/books?bibkeys=LCCN:${lccn}&jscmd=data&format=json`)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(json){
-        const idx = 0;
-        const key = Object.keys(json)[idx];
-        value = json[key]
-
-        const bookInfo = value
-        const poster = bookInfo.cover.large;
-        const title = bookInfo.title;
-        const author = bookInfo.authors[0].name;
-        const publisher = bookInfo.publishers[0].name;
-        const pages = bookInfo.number_of_pages;
-        const subjects = bookInfo.subjects[0].name;
-        const pubDate = bookInfo.publish_date;
-        const weight = bookInfo.weight;
-        const borrowable = bookInfo.ebooks[0].availability;
-        const preview_url = bookInfo.ebooks[0].preview_url;
-        const borrow_url = bookInfo.ebooks[0].borrow_url;
-
-        localStorage.setItem("title",title);
-        localStorage.setItem("poster",poster);
-        localStorage.setItem("author",author);
-        localStorage.setItem("publisher",publisher);
-        localStorage.setItem("pages",pages);
-        localStorage.setItem("subjects",subjects);
-        localStorage.setItem("pubDate",pubDate);
-        localStorage.setItem("weight",weight);
-        localStorage.setItem("borrowable", borrowable);
-        localStorage.setItem("preview_url",preview_url);
-        localStorage.setItem("borrow_url",borrow_url);
-
-
-        window.location = "bookData.html";
-
-    })
-}
-
-function showOLIDData(){
-    const olid = olidInput.value;
-    console.log(olid);
-    fetch(`https://openlibrary.org/api/books?bibkeys=OLID:${olid}&jscmd=data&format=json`)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(json){
-        const idx = 0;
-        const key = Object.keys(json)[idx];
-        value = json[key]
-
-        const bookInfo = value
-        const poster = bookInfo.cover.large;
-        const title = bookInfo.title;
-        const author = bookInfo.authors[0].name;
-        const publisher = bookInfo.publishers[0].name;
-        const pages = bookInfo.number_of_pages;
-        const subjects = bookInfo.subjects[0].name;
-        const pubDate = bookInfo.publish_date;
-        const weight = bookInfo.weight;
-        const borrowable = bookInfo.ebooks[0].availability;
-        const preview_url = bookInfo.ebooks[0].preview_url;
-        const borrow_url = bookInfo.ebooks[0].borrow_url;
-
-        localStorage.setItem("title",title);
-        localStorage.setItem("poster",poster);
-        localStorage.setItem("author",author);
-        localStorage.setItem("publisher",publisher);
-        localStorage.setItem("pages",pages);
-        localStorage.setItem("subjects",subjects);
-        localStorage.setItem("pubDate",pubDate);
-        localStorage.setItem("weight",weight);
-        localStorage.setItem("borrowable", borrowable);
-        localStorage.setItem("preview_url",preview_url);
-        localStorage.setItem("borrow_url",borrow_url);
-
-
-        window.location = "bookData.html";
-    })
-}
